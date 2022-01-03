be6b3501e04e695735b056d37ddc854512f01840
Jeremy Evans <jeremy@chiefgeek.io>
2022-01-03 15:14:41 -0500

    Init Commit

---
+++ MyFirstGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
